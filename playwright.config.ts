d7314fac462c797a27781079aa6a2ec7868f3059
sinaa2561321-design <sinaa.2561321@gmail.com>
2026-09-07 11:12:41 +0330

    Add files via upload

---
+++ playwright.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from "@playwright/test";
+export default defineConfig({
+  testDir: "./tests",
+  use: { baseURL: "http://127.0.0.1:3000", headless: true },
+  webServer: { command: "npm run dev", url: "http://127.0.0.1:3000", reuseExistingServer: true }
+});
